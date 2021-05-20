c80b775abde0ff1c4b67268ab8526d8792212184
Kashia Moua <kserenitym@gmail.com>
2021-05-20 00:00:56 -0500

    finished assignment

--- assignment/scripts/loops-practice.js
+++ assignment/scripts/loops-practice.js
@@ -51,9 +51,12 @@ console.log('---- 3. While loop ----');
 
 // 3.a. TODO: Write a while loop to console.log each star in the 'stars' array
 console.log('Some stars using while:');
-while(name of stars){
-  console.log(name);
+let index=0;
+while(index<stars.length){
+  console.log(stars[index])
+  index++;
 }
+
 // 3.b. TODO: Write a while loop to console.log the numbers from 0 to 5
 console.log('count from 0 to 5');
 let i=0
